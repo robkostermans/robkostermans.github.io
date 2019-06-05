9147322dbfb43aac8133854c4cd0b6a518e65bd3
Rob Kostermans <rob.kostermans@gmail.com>
2019-06-05 20:52:14 +0200

    cutout highlight

--- src/Containers/App/index.js
+++ src/Containers/App/index.js
@@ -29,7 +29,7 @@ class App extends Component {
 					hoverOffDelayInMs={150} //default: 0
 				>
 					<Holder theme="sketch" />
-					{/* <Holder theme="fill" /> */}
+					<Holder theme="fill" />
 				</ReactCursorPosition>
 			</section>
 		);
