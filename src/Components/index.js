69eb07b0158b43d6208b79096d45ea9de10735f9
Rob Kostermans <rob.kostermans@gmail.com>
2019-09-17 22:55:43 +0200

    first commit

--- src/Components/index.js
+++
@@ -1,5 +0,0 @@
-export * from './Pin';
-export * from './MainMap';
-export * from './Styling';
-export * from './List';
-export * from './ListItem';
--- src/Containers/index.js
+++
@@ -1,5 +0,0 @@
-export * from './App';
-export * from './Holder';
-export * from './Header';
-export * from './Main';
-export * from './Footer';
